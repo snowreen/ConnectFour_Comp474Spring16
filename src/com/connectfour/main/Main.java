2a4c18b05ae70d9064ffe7438f877fe50e878601
Syeda <Syeda@rubab.local>
2016-04-12 22:17:13 -0500

    Working version of connect 4 with reset option

--- src/com/connectfour/main/Main.java
+++ src/com/connectfour/main/Main.java
@@ -1,51 +1,69 @@
 package com.connectfour.main;
 
-import javax.swing.JFrame;
+import java.util.Scanner;
 
-import com.connectfour.model.*;
+import com.connectfour.model.Board;
+import com.connectfour.model.WinCheck;
 
 public class Main {
 
 	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
 	
-		Board board = new Board();
-		System.err.println("Board height = " + board.getHeight());
-		System.err.println("Board width = " + board.getWidth());
+		Board board = new Board(7, 7);
+		playConnectFour(input, board);
+	}
+
+	private static void playConnectFour(Scanner input, Board board) {
 		
-		board.set(new Checker(1), 0);
-		board.set(new Checker(1), 0);
-		board.set(new Checker(0), 0);
-		board.set(new Checker(0), 0);
-		board.set(new Checker(1), 0);
-		board.set(new Checker(1), 0);
-		board.set(new Checker(1), 0);
-		board.printBoard();
+		board.initializeBoard();
 		
-		Board board2 = new Board(10,10);
-		System.err.println("Board height = " + board.getHeight());
-		System.err.println("Board width = " + board.getWidth());
-		
-		board2.set(new Checker(1), 4);
-		board2.set(new Checker(1), 4);
-		board2.set(new Checker(0), 4);
-		board2.set(new Checker(0), 5);
-		board2.set(new Checker(1), 5);
-		board2.set(new Checker(1), 6);
-		board2.printBoard();
-		
-		Board board3 = new Board(20,20);
-		System.err.println("Board height = " + board.getHeight());
-		System.err.println("Board width = " + board.getWidth());
-		
-		for(int i = 0; i < 25; i++) {
-			board3.set(new Checker(1), 0);
-		}
-		board3.set(new Checker(1), 1);
-		board3.set(new Checker(0), 3);
-		board3.set(new Checker(0), 12);
-		board3.set(new Checker(1), 15);
-		board3.set(new Checker(1), 18);
-		board3.set(new Checker(1), 18);
-		board3.printBoard();
+		// This variable will alternate and mean whose turn is it. It is Red's turn now.
+        boolean isRed = true;
+        String value = null;
+        while (true) {
+        	System.out.println("Please type \"reset\" to restart the game anytime!");
+            if (isRed)
+                System.out.println("Red's turn now!");            
+            else 
+                System.out.println("Blue's turn now!");
+            System.out.print("Pick a spot from column 1 to 7:");
+            value = input.next();
+            if (value.equalsIgnoreCase("reset")) {
+            	break;
+            }
+            int column = Integer.parseInt(value);
+            if (column < 1 || column > 7) {
+                System.out.println("Column should be from 1 to 7");
+                continue;
+            }
+            if (!board.putColorChar(column - 1, isRed ? 'R' : 'B')) {
+                System.out.println("This column is filled! Choose another one.");
+                continue;
+            }
+
+            board.printBoard();
+            WinCheck winCheck = new WinCheck(board);
+            char result = winCheck.getWinner();
+            if (result == 'D') {
+                System.out.println("It is a draw!");
+                break;
+            }
+            else if (result == 'R') {
+                System.out.println("Red wins!");
+                break;
+            }
+            else if (result == 'B') {
+                System.out.println("Blue wins!");
+                break;
+            }
+            
+            isRed = !isRed;
+        }
+        
+        if (value.equalsIgnoreCase("reset")) {
+        	playConnectFour(input, board);
+        }
 	}
 }
--- src/com/connectfour/model/WinCheck.java
+++ src/com/connectfour/model/WinCheck.java
@@ -3,53 +3,170 @@ package com.connectfour.model;
 public class WinCheck {
 	
 	private Board board;
-	private boolean win = false;
+	private static final int CONNECT_WIN_CRITERIA = 4;
 	
 	public WinCheck(Board board) {
 		this.board = board;
 	}
 	
-	public boolean check() {
-		if (checkHorizontal()) { win = true; }
-		if (checkVertical()) { win = true; }
-		if (checkDiagonals()) { win = true; }
-		return win;
+	public char getWinner() {
+		char winner = checkHorizontal();
+        if (winner != ' ') {
+        	return winner;
+        }
+        winner = checkVertical();
+        if (winner != ' ') {
+        	return winner;
+        }
+        winner = checkDiagonals();
+        if (winner != ' ') {
+        	return winner;
+        }
+        
+        char[][] checker = board.getChecker();
+        
+        // Now we need to check if there are empty positions, otherwise it is a draw
+        for (int i = 0; i < board.getHeight(); ++i){
+        	for (int j = 0; j < board.getWidth(); ++j) {
+        		if (checker[i][j] == ' ') {
+        			return ' ';
+        		}
+        	}
+        }    
+
+        return 'D';
 	}
 	
-	private boolean checkHorizontal() {
-		int xp = board.getLastX();
-		int yp = board.getLastY();
-		
-		int count = 0;
-		int color = board.get(yp,xp).getColor();
-		
-		//Count consecutive pieces of the same color to the LEFT of initial checker
-		while (board.get(yp, xp).getColor() == color && count != 4) {
-			if (board.get(yp, xp).getColor() == color) {
-				count++;
-				xp--;
-			}
-			else {
-				break;
-			}
-		}
-		
-		//Count consecutive pieces of the same color to the RIGHT of initial checker
-		while (board.get(yp, xp).getColor() == color && count != 4) {
-			if (board.get(yp, xp).getColor() == color) {
-				count++;
-				xp--;
-			}
-			else {
-				break;
-			}
-		}
-		return false;
+	private char checkHorizontal() {
+		char[][] checker = board.getChecker();
+		for (int row = 0; row < board.getHeight(); ++row) {
+            int count = 0;
+            // Comparing current element with the previous
+            for (int column = 1; column < board.getWidth(); ++column) {
+                if (checker[row][column] != ' ' && checker[row][column] == checker[row][column-1]) {
+                	if (column == 1) {
+                		count++;
+                	}
+                	++count;
+                } else {
+                	count = 1;
+                }
+                
+                if (count >= CONNECT_WIN_CRITERIA) {
+                    return checker[row][column];
+                }
+            }
+        }
+        // Otherwise returning empty character, which means nobody win in rows.
+        return ' ';
 	}
-	private boolean checkVertical() {
-		return false;
+	
+	private char checkVertical() {
+		char[][] checker = board.getChecker();
+		for (int column = 0; column < board.getWidth(); ++column) {
+            int count = 0;
+            // Comparing current element with the previous
+            for (int row = 1; row < board.getHeight(); ++row) {
+                if (checker[row][column] != ' ' && checker[row][column] == checker[row-1][column]) {
+                	if (row == 1) {
+                		count++;
+                	}
+                	++count;
+                } else {
+                	count = 1;
+                }
+                if (count >= CONNECT_WIN_CRITERIA) {
+                    return checker[row][column];
+                }
+            }
+        }
+        // Otherwise returning empty character, which means nobody win in rows.
+        return ' ';
 	}
-	private boolean checkDiagonals() {
-		return false;
+	
+	private char checkDiagonals() {
+		
+		char[][] checker = board.getChecker();
+		
+		//check left to right upwards diagonals
+        char winner = leftToRightUpwards(checker);
+        if (winner != ' ') {
+        	return winner;
+        }
+        winner = leftToRightDownwards(checker);
+        return winner;
+	}
+	
+	private char leftToRightUpwards(char[][] checker) {
+		for (int row = board.getHeight() - 1; row >= CONNECT_WIN_CRITERIA - 1; --row) {
+            int count = 0;
+            // Comparing current element with the previous
+            int previousRow = row;
+            for (int column = 0; column < board.getWidth() - 1; ++column) {
+            	if (!((board.getWidth() - column) >= (CONNECT_WIN_CRITERIA - count))) {
+            		break;
+            	}
+                if (checker[previousRow][column] != ' ' && checker[previousRow - 1][column + 1] != ' ') {
+                	if (checker[previousRow - 1][column + 1] == checker[previousRow][column]) {
+                		if (column == 0 || (column > 0 && previousRow < board.getHeight() - 1 
+                				&& (checker[previousRow + 1][column - 1] == ' ' || checker[previousRow + 1][column - 1] != checker[previousRow][column]))) {
+                    		count++;
+                    	}
+                    	count++;
+                    	if (count >= CONNECT_WIN_CRITERIA) {
+                        	return checker[previousRow][column];
+                        }
+                	} else {
+                    	count = 0;
+                    }
+                } else {
+                	count = 0;
+                }
+                previousRow--;
+                if (previousRow < 1) {
+            		break;
+            	}
+                
+            }
+        }
+		// Otherwise returning empty character, which means nobody win in left to right upwards diagonal
+        return ' ';
+	}
+	
+	private char leftToRightDownwards(char[][] checker) {
+		for (int row = 0; row <= board.getHeight() - CONNECT_WIN_CRITERIA; ++row) {
+            int count = 0;
+            // Comparing current element with the next
+            int nextRow = row;
+            for (int column = 0; column < board.getWidth() - 1; ++column) {
+            	
+            	if (!((board.getWidth() - column) >= (CONNECT_WIN_CRITERIA - count))) {
+            		break;
+            	}
+                if (checker[nextRow][column] != ' ' && checker[nextRow + 1][column + 1] != ' ') {
+                	if (checker[nextRow + 1][column + 1] == checker[nextRow][column]) {
+                		if (column == 0 || (column > 0 && nextRow > 0 
+                				&& (checker[nextRow - 1][column - 1] == ' ' || checker[nextRow - 1][column - 1] != checker[nextRow][column]))) {
+                    		count++;
+                    	}
+                    	count++;
+                    	if (count >= CONNECT_WIN_CRITERIA) {
+                        	return checker[nextRow][column];
+                        }
+                	} else {
+                    	count = 0;
+                    }
+                } else {
+                	count = 0;
+                }
+                nextRow++;
+                if (nextRow >= board.getHeight() - 1) {
+            		break;
+            	}
+                
+            }
+        }
+		// Otherwise returning empty character, which means nobody win in left to right downwards diagonal
+        return ' ';
 	}
 }
