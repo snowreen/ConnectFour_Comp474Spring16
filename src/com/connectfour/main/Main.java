1b1bb74810ff8efde225fa410d6c556ca32f02a4
J_Banks <jbanks1@luc.edu>
2016-04-14 19:11:20 -0500

    Working on WinCheck
    Still trying to solve endgame algorithms for finding win condition

--- src/com/connectfour/main/Main.java
+++ src/com/connectfour/main/Main.java
@@ -12,40 +12,43 @@ public class Main {
 		System.err.println("Board height = " + board.getHeight());
 		System.err.println("Board width = " + board.getWidth());
 		
-		board.set(new Checker(1), 0);
-		board.set(new Checker(1), 0);
-		board.set(new Checker(0), 0);
-		board.set(new Checker(0), 0);
-		board.set(new Checker(1), 0);
-		board.set(new Checker(1), 0);
-		board.set(new Checker(1), 0);
+		board.set(new Checker(1), 1);
+		board.set(new Checker(0), 2);
+		board.set(new Checker(1), 3);
+		board.set(new Checker(1), 4);
 		board.printBoard();
 		
-		Board board2 = new Board(10,10);
-		System.err.println("Board height = " + board.getHeight());
-		System.err.println("Board width = " + board.getWidth());
+		WinCheck wc = new WinCheck(board);
 		
-		board2.set(new Checker(1), 4);
-		board2.set(new Checker(1), 4);
-		board2.set(new Checker(0), 4);
-		board2.set(new Checker(0), 5);
-		board2.set(new Checker(1), 5);
-		board2.set(new Checker(1), 6);
-		board2.printBoard();
+		System.out.println("Game status: " + wc.check());
 		
-		Board board3 = new Board(20,20);
-		System.err.println("Board height = " + board.getHeight());
-		System.err.println("Board width = " + board.getWidth());
 		
-		for(int i = 0; i < 25; i++) {
-			board3.set(new Checker(1), 0);
-		}
-		board3.set(new Checker(1), 1);
-		board3.set(new Checker(0), 3);
-		board3.set(new Checker(0), 12);
-		board3.set(new Checker(1), 15);
-		board3.set(new Checker(1), 18);
-		board3.set(new Checker(1), 18);
-		board3.printBoard();
+		
+//		Board board2 = new Board(10,10);
+//		System.err.println("Board height = " + board.getHeight());
+//		System.err.println("Board width = " + board.getWidth());
+//		
+//		board2.set(new Checker(1), 4);
+//		board2.set(new Checker(1), 4);
+//		board2.set(new Checker(0), 4);
+//		board2.set(new Checker(0), 5);
+//		board2.set(new Checker(1), 5);
+//		board2.set(new Checker(1), 6);
+//		board2.printBoard();
+//		
+//		Board board3 = new Board(20,20);
+//		System.err.println("Board height = " + board.getHeight());
+//		System.err.println("Board width = " + board.getWidth());
+//		
+//		for(int i = 0; i < 25; i++) {
+//			board3.set(new Checker(1), 0);
+//		}
+//		board3.set(new Checker(1), 1);
+//		board3.set(new Checker(0), 3);
+//		board3.set(new Checker(0), 12);
+//		board3.set(new Checker(1), 15);
+//		board3.set(new Checker(1), 18);
+//		board3.set(new Checker(1), 18);
+//		board3.printBoard();
 	}
 }
--- src/com/connectfour/model/WinCheck.java
+++ src/com/connectfour/model/WinCheck.java
@@ -10,46 +10,52 @@ public class WinCheck {
 	}
 	
 	public boolean check() {
-		if (checkHorizontal()) { win = true; }
-		if (checkVertical()) { win = true; }
-		if (checkDiagonals()) { win = true; }
+		checkHorizontal();
+		checkVertical();
+		checkDiagonals();
 		return win;
 	}
 	
-	private boolean checkHorizontal() {
+	private void checkHorizontal() {
+		
+		//initialize x and y pointers to origin of last checker
 		int xp = board.getLastX();
 		int yp = board.getLastY();
 		
 		int count = 0;
 		int color = board.get(yp,xp).getColor();
 		
-		//Count consecutive pieces of the same color to the LEFT of initial checker
-		while (board.get(yp, xp).getColor() == color && count != 4) {
-			if (board.get(yp, xp).getColor() == color) {
-				count++;
-				xp--;
-			}
-			else {
-				break;
-			}
-		}
+//		//Count consecutive pieces of the same color to the LEFT of initial checker
+//		while (xp > 0 && count != 4) {
+//			if (board.get(yp, xp) != null) {
+//				if (board.get(yp, xp).getColor() == color) {
+//					count++;
+//					xp--;
+//				} else {
+//					break;
+//				}
+//			}
+//		}
+//		
+//		//Reset x and y pointers to check neighbor checkers to right of origin
+//		xp = board.getLastX();
+//		yp = board.getLastY();
+//		
+//		//Count consecutive pieces of the same color to the RIGHT of initial checker
+//		while (xp < board.getWidth() && count != 4) {
+//			if (board.get(yp, xp) != null && board.get(yp, xp).getColor() == color) {
+//				count++;
+//				xp++;
+//			}
+//		}
+//		if (count == 4) { win = true; }
 		
-		//Count consecutive pieces of the same color to the RIGHT of initial checker
-		while (board.get(yp, xp).getColor() == color && count != 4) {
-			if (board.get(yp, xp).getColor() == color) {
-				count++;
-				xp--;
-			}
-			else {
-				break;
-			}
-		}
-		return false;
 	}
-	private boolean checkVertical() {
-		return false;
+	
+	private void checkVertical() {
+		return;
 	}
-	private boolean checkDiagonals() {
-		return false;
+	private void checkDiagonals() {
+		return;
 	}
 }
